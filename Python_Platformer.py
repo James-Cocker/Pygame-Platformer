b8542e482e2d4a4b447b3fe6b40f1d312ccafb5b
James Cocker <james.s.earth@gmail.com>
2022-11-26 11:53:28 +0000

    Level Saving + Debugging

--- Python_Platformer.py
+++ Python_Platformer.py
@@ -28,22 +28,30 @@ mixer.music.play()
 # This is in the format [ No. of lives (between 1 and 5), double jump collected?, dash collected? ]
 PlayerLivesAndAbilities = [5, False, False]
 
+# Routine to load and return the next level automatically after the prvious has been completed
+def MoveToNextLevel(CurrentLevelNum, PlayerLivesAndAbilities, MaxLevelReached):
+    # Increase max level num if they are progressing through the game (to prevent incrementing by one when player is replaying old levels)
+    CurrentLevelNum += 1
+    if MaxLevelReached <= CurrentLevelNum:
+        MaxLevelReached = CurrentLevelNum
+    TempToDisableTimer = CurrentLevel.ToDisableTimer
+    CSVPath = 'Levels/Level ' + str(CurrentLevelNum) + '/Level ' + str(CurrentLevelNum) + '.csv'
+    return Level(ImportCSV(CSVPath), screen, CurrentLevelNum, ProgrammerMode, InGameMenu, TempToDisableTimer, PlayerLivesAndAbilities), CurrentLevelNum, MaxLevelReached
+
+
+# --- Name Screen ---
+
+Name = DisplayNameScreen(screen)
+MaxLevelReached,PlayerID = LoadLevelsReached(Name)
+
 # Create Menus (only needs to be created once each time the program is loaded)
 TitleScreen = CreateTitleScreen(screen)
 LevelSelectionScreen = CreateLevelSelectionScreen(screen)
 InGameMenu = CreateInGameMenu((300,100), screen)
 
-# Routine to load and return the next level automatically after the prvious has been completed
-def MoveToNextLevel(CurrentLevelNum, PlayerLivesAndAbilities):
-    TempToDisableTimer = CurrentLevel.ToDisableTimer
-    CurrentLevelNum += 1
-    CSVPath = 'Levels/Level ' + str(CurrentLevelNum) + '/Level ' + str(CurrentLevelNum) + '.csv'
-    return Level(ImportCSV(CSVPath), screen, CurrentLevelNum, ProgrammerMode, InGameMenu, TempToDisableTimer, PlayerLivesAndAbilities), CurrentLevelNum
 
-# Name Screen
-Name = DisplayNameScreen(screen)
+# --- Title Screen ---
 
-# Title Screen
 while StartedGame == False:
     for event in pygame.event.get():
         if event.type==pygame.QUIT: sys.exit()
@@ -53,9 +61,10 @@ while StartedGame == False:
     screen.fill((11, 11, 11))    # Remove the previous frame we drew on the screen by filling it with black (so they do not overlap)
     TitleScreen.update()
 
+    # Loop through each button in title screen and check for 'start' or 'load game'
     for button in TitleScreen.Buttons:
         if button.Name == 'Start' and button.Clicked:
-            CurrentLevelNum = 0
+            CurrentLevelNum = MaxLevelReached
             StartedGame = True
             break
         elif button.Name == 'Load Game' and button.Clicked:
@@ -65,16 +74,24 @@ while StartedGame == False:
     pygame.display.update()
     Clock.tick(60)
 
-# The indefinite loop when the player hasselected 'start' or 'load game'
+
+# The indefinite loop when the player has selected 'start' or 'load game'
 while True:
-    # Level selection screen
+
+    # --- Level selection screen ---
+
+    # Give this its own routine so that when user 'returns' back to level selection screen, their max level reached (and hence the buttons available to them) have updated
+    CreateButtonsForLevelSelectionScreen(int(MaxLevelReached), LevelSelectionScreen)
+
     while CurrentLevelNum == -1:
         for event in pygame.event.get():
-            if event.type==pygame.QUIT: sys.exit()
+            if event.type==pygame.QUIT:
+                SaveScores(MaxLevelReached, PlayerID)
+                sys.exit()
             elif event.type==pygame.MOUSEBUTTONDOWN:
                 LevelSelectionScreen.MouseDown = True
 
-        screen.fill((11, 11, 11))   # Same as above
+        screen.fill((11, 11, 11))
         LevelSelectionScreen.update()
 
         for button in LevelSelectionScreen.Buttons:
@@ -92,11 +109,14 @@ while True:
     CurrentLevel = Level(ImportCSV(CSVPath), screen, CurrentLevelNum, ProgrammerMode, InGameMenu, False, PlayerLivesAndAbilities)
 
 
-    # In-game loop
+    # --- In-game loop ---
+
     while InGameMenu.Return == False:
         # check if we pressed quit
         for event in pygame.event.get():
-            if event.type==pygame.QUIT: sys.exit()
+            if event.type==pygame.QUIT:
+                SaveScores(MaxLevelReached, PlayerID)
+                sys.exit()
             elif event.type==pygame.MOUSEBUTTONDOWN:
                 InGameMenu.MouseDown = True
             # Create an in-game menu if escape is pressed
@@ -118,11 +138,16 @@ while True:
         # Running the next level when the player compltes the first one
         if CurrentLevel.FinishedLevel == False:
             CurrentLevel.run()               
-        elif CurrentLevelNum < NumberOfLastLevel:
-            CurrentLevel, CurrentLevelNum = MoveToNextLevel(CurrentLevelNum, PlayerLivesAndAbilities)
+        elif int(CurrentLevelNum) < NumberOfLastLevel:
+            CurrentLevel, CurrentLevelNum, MaxLevelReached = MoveToNextLevel(int(CurrentLevelNum), PlayerLivesAndAbilities, int(MaxLevelReached))
         else:
             # Display winning screen and return back to menu
             continue
+
+        # Save and exit if the user has chosen to quit the game from pressing 'exit' in the in-game menu
+        if CurrentLevel.SaveAndExit:
+            SaveScores(MaxLevelReached, PlayerID)
+            sys.exit()
 
         # Update the screen and keep the frame rate at 60
         pygame.display.update()
--- Support.py
+++ Support.py
@@ -46,11 +46,60 @@ def DisplayNameScreen(screen):
         screen.fill((11, 11, 11))
         screen.blit(BackgroundImg, (0,0))
 
-        OutputText = Font.render(NameText, True, (160,160,160))
+        OutputText = Font.render(NameText, True, (20,20,20))
         screen.blit(OutputText, (528,274))
 
         pygame.display.update()
         Clock.tick(60)
 
-def SaveScores():
+def ReadFile():
+    with open("Players/Players.txt") as PlayersFile:
+        Lines = [line.strip().split(",") for line in PlayersFile]
+    
+    return Lines
+
+def SaveUpdatedFile(Lines):
+    with open('Players/Players.txt', mode='w') as PlayersFile:
+        FileText = ""
+        for PlayerInfo in Lines:
+            for Element in PlayerInfo:
+                FileText += str(Element) + ","
+            FileText = FileText[:len(FileText)-1]       # Taking off last comma
+            FileText += "\n"
+        
+        # Remove the last character ("\") from the text to be written to the txt file - the "n" was already taken off by the last comma shortening
+        FileText = FileText[:len(FileText)-1]
+        PlayersFile.write(FileText)
+
+def LoadLevelsReached(Name):
+    Name = Name.upper()         # Convert name to uppercase so case won't matter with text input
+    ListNum = 0                 # To identify which list in the 2d array we are on, so that the player's ID can be saved
+
+    # Open the file and see if the user's name can be found, in which case save their row number (ID) and return their max level reached
+    Lines = ReadFile()
+
+    for PlayerInfo in Lines:
+        if PlayerInfo[0] == Name:
+            PlayerID = ListNum
+            return PlayerInfo[1], PlayerID
+        ListNum += 1
+    # Otherwise set the player's ID to the next line in the csv file
+    PlayerID = ListNum
+
+    # In the format: Name, max level reached, time taken for level 1, golden gear collected? (T/F), time taken for level 2, golden gear collected? (T/F), ect...
+    Lines.append([Name,0,-1,False,-1,False,-1,False,-1,False,-1,False,-1,False,-1,False,-1,False,-1,False])
+
+    SaveUpdatedFile(Lines)
+
+    # If player has just created new entry in the csv then report their max level as 0 and return their ID in table (row num)
+    return 0, PlayerID
+
+def LoadScores():
     print()
+
+def SaveScores(MaxLevelReached, PlayerID):
+    # Open the file and see if the user's name can be found, in which case save their row number (ID) and return their max level reached
+    Lines = ReadFile()
+    Lines[PlayerID][1] = MaxLevelReached
+
+    SaveUpdatedFile(Lines)
